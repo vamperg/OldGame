54b8fb9a56c39f65cd5cc181e6ccdbd6062910bc
vamperg <80493391+vamperg@users.noreply.github.com>
2021-03-12 10:38:32 +0300

    Add files via upload

---
+++ Program (1).cs
@@ -0,0 +1,457 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace LearnGame
+{
+    class Program
+    {
+        
+        static int code, pun, money, ore;
+        static string mydocu = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        static int[] pickaxes = new int[3] {0, 0, 0 };
+        static int[] woodpix = new int[10] { 100, 200, 300, 400, 600, 800, 1000, 1400, 1800, 2200 };
+        static int[] ironpix = new int[10] { 3000, 3500, 4100, 5000, 6000, 8000, 10000, 11400, 12800, 14000 };
+        static int[] goldpix = new int[10] { 15500, 17000, 20300, 22400, 24600, 26800, 28100, 31400, 33800, 39990 };
+        static int[] pixcount = new int[3];
+        static int[] pixcountsave = new int[3] { 10, 30, 50 };
+        static double[] pixtime = new double[3] { 0.2, 0.6, 1.2 };
+        static char[] kur = new char[255];
+        public static string nickname;
+        static Random rnd = new Random();
+
+        static void Sleep(double persec)
+        {
+            double per = persec * 1000;
+            Thread.Sleep((int)per);
+        }
+
+        static void Clear()
+        {
+            Console.Clear();
+        }
+        static void Exit()
+        {
+            Clear();
+            Console.Write("Удачи...");
+            Sleep(0.5);
+            Environment.Exit(0);
+        }
+        static void Stats()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Имя:{0}, Денег:{1}, Руды:{2}", nickname, money, ore);
+            Console.ResetColor();
+        }
+        static void Help()
+        {
+            Clear();
+            Stats();
+            Console.Write("(Помощь)\n\nЭто подобие кликера, но при этом на консоле.\nКак вы могли видеть в меню есть пункт 'Копать'.\nТам вы копаете руду и уже в магазине эту руду можно продать.\nТам же можно купить более сильное оружие.\nРазработчик Линар Галяув.\nНажмите любую кнопку, чтобы вернуться в меню...");
+            Console.ReadKey();
+            Menu();
+            
+        }
+        static void ShopMax()
+        {
+            Clear();
+            Console.Write("Кирка достигла максимального уровня!\n>Назад");
+            Console.ReadKey();
+            Shop();
+        }
+        static void ShowLowMoney()
+        {
+            Clear();
+            Console.Write("У вас недостаточно денег!\n>Назад");
+            Console.ReadKey();
+            ShopPickaxe();
+        }
+        static void ShopPickaxe()
+        {
+            code = 0; pun = 1; kur[0] = '>';
+            do
+            {
+                Console.Clear();
+                Stats();
+                Console.Write("(Кирки)\t\t(Уровень кирки)\t(Цена)\t(Время добычи)\t(Кол-во руды)\n\n");
+                Console.Write("{0}Простая кирка\t\t({7})\t{1}\t-0.2сек.\t+5-10\n{2}Железная кирка\t\t({8})\t{3}\t-0.6 сек.\t+20-30\n{4}Профессиональная кирка\t({9})\t{5}\t-1,2сек.\t+40-50\n{6}Назад", kur[0],(woodpix[pickaxes[0]]),kur[1],(ironpix[pickaxes[1]]),kur[2],(goldpix[pickaxes[2]]),kur[3],pickaxes[0],pickaxes[1],pickaxes[2]);
+                code = Kur(4, "");
+            } while (code == 0);
+            if (code == 1)
+            {
+                if ((money >= woodpix[pickaxes[0]]) && (pickaxes[0] <9))
+                {
+                    money -= (woodpix[pickaxes[0]]);
+                    pickaxes[0] += 1;
+                    pixcount[0] += 10;
+                    ShopPickaxe();
+                }
+                else if(money < woodpix[pickaxes[0]])
+                {
+                    ShowLowMoney();
+                }
+                else if (pickaxes[0] == 9)
+                {
+                    ShopMax();
+                }
+            }
+            else if(code == 2)
+            {
+                if ((money >= ironpix[pickaxes[1]]) && (pickaxes[1] < 9))
+                {
+                    money -= (ironpix[pickaxes[1]]);
+                    pickaxes[1]++;
+                    pixcount[1] += 30;
+                    ShopPickaxe();
+                }
+                else if (money < ironpix[pickaxes[1]])
+                {
+                    ShowLowMoney();
+                }
+                else if (pickaxes[1] == 9)
+                {
+                    ShopMax();
+                }
+            }
+            else if (code == 3)
+            {
+                if ((money >= goldpix[pickaxes[2]]) && (pickaxes[2] < 9))
+                {
+                    money -= (goldpix[pickaxes[2]]);
+                    pickaxes[2]++;
+                    pixcount[2] += 50;
+                    ShopPickaxe();
+                }
+                else if (money < goldpix[pickaxes[2]])
+                {
+                    ShowLowMoney();
+                }
+                else if (pickaxes[2] == 9)
+                {
+                    ShopMax();
+                }
+            }
+            else
+            {
+                Shop();
+            }
+        }
+        static void Shop()
+        {
+            code = 0; pun = 1; kur[0] = '>';
+            do
+            {
+                Clear();
+                Stats();
+                Console.Write("(Магазин)\n\n{0}Купить/Улучшить Кирку\n{1}Продать руду(1=4$)\n{2}Назад", kur[0], kur[1], kur[2]);
+                code = Kur(3, "");
+            } while (code == 0);
+            if (code == 1)
+            {
+                ShopPickaxe();
+            }
+            else if (code == 2)
+            {
+                money += (ore * 4);
+                ore = 0;
+                Shop();
+            }
+            else Menu();
+        }
+        static int Kur(int maxi, string target)
+        {
+            ConsoleKey btn = Console.ReadKey().Key;
+            if (btn == ConsoleKey.Enter)
+            {
+                kur[pun-1] = ' ';
+                return pun;
+            }
+            if (btn == ConsoleKey.W || btn == ConsoleKey.UpArrow)
+            {
+                if (pun == 1)
+                {
+                    kur[0] = ' ';
+                    pun = maxi;
+                    kur[maxi - 1] = '>';
+                    return 0;
+                }
+                else
+                {
+                    kur[pun - 1] = ' ';
+                    kur[pun - 2] = '>';
+                    pun--;
+                    return 0;
+                }
+            }
+            else if (btn == ConsoleKey.S || btn == ConsoleKey.DownArrow)
+            {
+                if (pun == maxi)
+                {
+                    kur[maxi - 1] = ' ';
+                    pun = 1;
+                    kur[0] = '>';
+                    return 0;
+                }
+                else
+                {
+                    kur[pun - 1] = ' ';
+                    kur[pun] = '>';
+                    pun++;
+                    return 0;
+                }
+            }
+            else return 0;
+        }
+        //Угольная шахта
+        static void CoalMine()
+        {
+            double worktime = 20;
+            int orecount = 0;
+            if (pickaxes[0] == 0)
+            {
+                Clear();
+                Console.Write("Для работы в шахте вам нужно купить инструменты!\n>Назад");
+                Console.ReadKey();
+                Menu();
+            }
+            else
+            {
+                for (int i = 0; i < pickaxes.Length; i++)
+                {
+                    worktime -= pickaxes[i] * pixtime[i];
+                    if (pickaxes[i] != 0)
+                    {
+                        orecount += rnd.Next(5, pixcount[i]);
+                    }
+                }
+                ore += orecount;
+                for (double i = 0; i < worktime; i+=0.2)
+                {
+                    Clear();
+                    Console.Write("Добыча руды[{0}/{1}]", i, worktime);
+                    Sleep(0.2);
+                }
+                pun = 1; code = 0; kur[0] = '>';
+                do
+                {
+                    Clear();
+                    Stats();
+                    Console.Write("Добыто {0} Руды! Можете продать ее в магазине.\n\n{1}Продолжить\n{2}Назад", orecount, kur[0], kur[1]);
+                    code = Kur(2, "");
+                } while (code == 0);
+                if (code == 1)
+                {
+                    CoalMine();
+                }
+                else
+                {
+                    OreMine();
+                }
+            }
+        }
+
+        //Шахты
+        static void OreMine()
+        {
+            code = 0; pun = 1; kur[0] = '>';
+            do
+            {
+                Clear();
+                Stats();
+                Console.Write("(Шахты)\n\n{0}Угольная шахта\n{1}В меню",kur[0],kur[1]);
+                code = Kur(2, "");
+            } while (code == 0);
+            if (code == 1)
+            {
+                CoalMine();
+            }
+            if (code == 2)
+            {
+                Menu();
+            }
+        }
+
+
+        static void NewGame()
+        {
+            Console.Write("Добро пожаловать в игру.\nВведите свой ник:");
+            nickname = Console.ReadLine();
+            money = 100;
+            ore = 0;
+            Clear();
+            Menu();
+        }
+        static bool IsLoad()
+        {
+            FileInfo file = new FileInfo(mydocu + "/Mining Business/save.ini");
+
+            if (file.Exists)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void SwitchStart(int number)
+        {
+            Clear();
+            switch (number)
+            {
+                case 1: NewGame();
+                    break;
+                case 2: LoadGame();
+                    break;
+            }
+        }
+        static void Start()
+        {
+            int sw = 0;
+            code = 0; pun = 1; kur[0] = '>';
+            do
+            {
+                Clear();
+               if (IsLoad())
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("{0}Загрузить\n", kur[0]);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("{0}Новая игра", kur[1]);
+                    Console.ResetColor();
+                    sw = 1;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("{0}Новая игра\n", kur[0]);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("{0}Загрузить", kur[1]);
+                    Console.ResetColor();
+                    sw = 2;
+                };
+               
+                Console.Write("\n{0}Выход", kur[2]);
+                Console.ResetColor();
+                code = Kur(3, "");
+            } while (code == 0);
+            if (code == 1)
+            {
+                if (sw == 1)
+                {
+                    SwitchStart(2);
+                }
+                else SwitchStart(1);
+            }
+            else if (code == 2)
+            {
+                if (sw == 1)
+                {
+                    SwitchStart(sw);
+                }
+                else SwitchStart(2);
+            }
+            else
+            {
+                Exit();
+            }
+        }
+        static void SaveGame(object args)
+        {
+            FileInfo file = new FileInfo(mydocu + "/Mining Business/save.ini");
+
+            Sleep(0.2);
+            string[] save = new string[6] { nickname, money.ToString(), ore.ToString(), pickaxes[0].ToString(), pickaxes[1].ToString(), pickaxes[2].ToString() };
+            File.WriteAllLines(mydocu + "/Mining Business/save.ini", save);
+        }
+        static void LoadGame()
+        {
+            if (IsLoad())
+            {
+                code = 0; pun = 1; kur[0] = '>';
+                do
+                {
+                    Clear();
+                    Console.Write("Есть сохранение!\nSave: {0}\n{1}Загрузить\n{2}Удалить", File.GetCreationTime(mydocu + "/Mining Business/save.ini").ToString(), kur[0], kur[1]);
+                    code = Kur(2, "");
+                } while (code == 0);
+                if (code == 1)
+                {
+                    string[] load = File.ReadAllLines(mydocu + "/Mining Business/save.ini", Encoding.UTF8);
+                    nickname = load[0];
+                    money = int.Parse(load[1]);
+                    ore = int.Parse(load[2]);
+                    for (int i = 0; i < pickaxes.Length; i++)
+                    {
+                        pickaxes[i] = int.Parse(load[3 + i]);
+                        pixcount[i] += pickaxes[i] * pixcountsave[i];
+                    }
+                    Menu();
+                }else if (code == 2)
+                {
+                    File.Delete(mydocu + "/Mining Business/save.ini");
+                    Clear();
+                    Console.Write("Сохранение было удалено!");
+                    Console.ReadKey();
+                    Start();
+                }
+            }              
+            else
+            {            
+                pun = 1; code = 0; kur[0] = '>';
+                do
+                {
+                    Clear();
+                    Console.WriteLine("Сохранение не найдено!\n{0}Назад",kur[0]);
+                    code = Kur(1, "");
+                } while (code == 0);
+                if (code == 1)
+                {
+                    Start();
+                }
+            }
+        }
+
+        static void Menu()
+        {
+            Timer timetosave = new Timer(SaveGame, null, 0, 10 * 1000);
+            code = 0; pun = 1; kur[0] = '>';
+            do
+            {
+                Clear();
+                Stats();
+                Console.Write("(Меню)\n\n{0}Копать\n{1}Магазин\n{2}Помощь\n{3}Сохранить и выйти", kur[0], kur[1], kur[2], kur[3]);
+                code = Kur(4, "");
+            } while (code == 0);
+            if (code == 1)
+            {
+                OreMine();
+            }
+            else if (code == 2)
+            {
+                Shop();
+            }
+            else if (code == 3)
+            {
+                Help();
+            }
+            else if (code == 4)
+            {
+                SaveGame(2);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(mydocu+"/Mining Business");
+            Console.SetWindowSize(100,30);
+            Console.Title = "Mining Business";
+            Start();
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Controller;
+using LoadMap;
+
+namespace Game
+{
+    
+    public class Program
+    {
+        
+        public static int Width, Height;
+        public static int xp = 12;
+        public static int yp = 7;
+        static int chestx;
+        static int chesty;
+        static int traderx,tradery;
+        static int Hp = 100;
+        static int Lvl = 1;
+        static int Score = 1;
+        static int Money = 0;
+        static bool TakeChest = true;
+        static char[][] Map;
+        const int CurX = 30;
+        static string[] Loot = {"Пусто", "Ножны", "Обломки брони","Футболка","Свежие трусы","Золотые зубы","Безобразное ожирелье","Игрушка обезьянка с дисками","Безвкусная ваза","Убийственнокрасивый меч" };
+        static string[] FTrader = { "Что сегодня хочешь продать мне?", "Салам Пету...кхе, что хотите продать?", "Я сегодня щедрый, давай сюда свой хлам", "О, снова ты, молчаливый искатель" };
+        public static int[] Inventary = new int[12];
+        //-----------------------------------------------------------------------------------------------\\
+        
+        //-----------------------------------------------------------------------------------------------\\
+        public static void CurPos(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+        }
+        static void CreateChest()
+        {
+            Random rnd = new Random();
+            chestx = rnd.Next(2, Width-1);
+            chesty = rnd.Next(2, Height-1);
+            TakeChest = true;
+        }
+        static void InvSort()
+        {
+            for (int i =0; i<Inventary.Length; i++)
+            {
+                for (int j = 0; j < Inventary.Length-1; j++)
+                {
+                    if (Inventary[j] < Inventary[j + 1])
+                    {
+                        int temp = Inventary[j];
+                        Inventary[j] = Inventary[j + 1];
+                        Inventary[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        static int Rand(int min, int max)
+        {
+            Random rnd = new Random();
+            int a = rnd.Next(min, max);
+            return a;
+        }
+
+        public static void Trader()
+        {
+            Console.Clear();
+            CurPos(0, 18);
+            int Sum=0, code;
+            string Text = FTrader[Rand(0,4)];
+            int[] mas = new int[12];
+            Random rnd = new Random();
+            for (int i = 0; i<Inventary.Length; i++)
+            {
+                if (Inventary[i] != 0)
+                {
+                    int a = rnd.Next(Lvl, 2*Inventary[i]);
+                    mas[i] = a;
+                    Sum += mas[i];
+                }
+            }
+            Control.pun = 1; Control.code = 0; Control.kur[0] = '>';
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("--------------Лавка Торговца--------------\nТорговец:{0}", Text);
+                Console.Write("{0}Продать весь хлам({2} монет)\n{1}Молча уйти", Control.kur[0], Control.kur[1], Sum);
+                code = Control.ChatKur(2);
+            } while (code == 0);
+            if (code == 1)
+            {
+                Money += Sum;
+                for (int i = 0; i < Inventary.Length; i++)
+                {
+                    Inventary[i] = 0; 
+                }
+                Draw();
+            }
+            else
+            {
+                xp++;
+                Console.Clear();
+            }
+        }
+        static string InvName(int i)
+        {
+            return Loot[Inventary[i]];
+        }
+        static void FullInv()
+        {
+            if (Inventary[11] != 0)
+            {
+                CurPos(0, 16);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Инвентарь полон!");
+
+            }
+            else Console.ForegroundColor = ConsoleColor.DarkGreen;
+        }
+        static void SetSize()
+        {
+            Height = Map.Length;
+            Width = Map[0].Length;
+        }
+        static void DrawInv()
+        {
+            InvSort();
+            CurPos(CurX, 0);
+            Console.Write("Инвентарь");
+            CurPos(CurX, 1);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            if (Inventary[0] == 0)
+            {
+                Console.Write("Сейчас у вас ничего нет");
+            }
+            else
+            {
+                int a = 0;
+                int inc = 1;
+                for (int i = 0; i < Inventary.Length; i++)
+                {
+                    CurPos(CurX, 1 + a);
+                    if (i <= 10)
+                    {
+                        if (Inventary[i] == Inventary[i + 1])
+                        {
+                            inc++;        
+                        }
+                        else if (inc>1)
+                        {
+                            Console.Write("{0}x[{1}]",inc,InvName(i));
+                            a++;
+                            inc = 1;
+                        }
+                        else
+                        {
+                            Console.Write("[{0}]", InvName(i));
+                            a++;
+                        }
+                    }
+                    else if (i==11 && Inventary[i] != Inventary[i-1])
+                    {
+                        if (Inventary[i] != 0)
+                        {
+                            Console.Write("[{0}]", InvName(i));
+                            a++;
+                        }
+                    }
+                    else if (Inventary[i] !=0)
+                    {
+                        Console.Write("{0}x[{1}]", inc, InvName(i));
+                        inc = 1;
+                    }
+
+                }                               
+            }
+        }
+
+        static void DrawText()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("Hp:{0},Money:{4}, Level:{1}({2}/{3})",Hp, Lvl,Score, Score*3, Money);            
+
+        }
+
+        static void Draw()
+        {
+            while (true)
+            {
+                
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;
+                
+                for (int y = 0; y < Map.Length; y++)
+                {
+                    Console.SetCursorPosition(0, y);
+                    for (int x = 0; x < Map[0].Length; x++)
+                    {
+                        var map = Map[y][x];
+                        if (x == xp && y == yp)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            Console.SetCursorPosition(x, y);
+                            Console.Write("1");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        else if(xp == chestx && yp == chesty && TakeChest)
+                        {
+                            RandLoot(2);
+                        }               
+                        else if(xp == traderx && yp == tradery)
+                        {
+                            Trader();
+                        }
+                        else if ('1' == map)
+                        {
+                            CurPos(x, y);
+                            Console.Write("#");
+                        }else if ('T' == map)
+                        {                            
+                            CurPos(x, y);
+                            traderx = x;
+                            tradery = y;
+                            Console.Write("T");
+                            
+                        }
+                        else if (x == chestx && y == chesty && TakeChest)
+                        {
+                            Console.SetCursorPosition(x, y);
+                            Console.Write("C");
+                        }
+                    }
+                    Console.WriteLine();
+                }               
+                DrawText();
+                FullInv();
+                DrawInv();
+                Control.KeyPlayer();
+            }
+        }
+
+        static void RandLoot(int Type)
+        {
+            Random rnd = new Random();
+            InvSort();
+            if (Type == 1)//Монстры
+            {
+                int a = rnd.Next(4);
+                for (int i = 0; i < Inventary.Length; i++)
+                {
+                    if (Inventary[i] == 0 && a != 0)
+                    {
+                        Inventary[i] = rnd.Next(1, 9);
+                        a--;
+                    }
+                    if (Inventary[11] != 0)
+                    {
+                        break;
+                    }
+                }
+                
+            }
+
+            else if (Type == 2)//Сундук
+            {
+                int a = rnd.Next(1, 6);
+                int[] arr = new int[a];
+                for (int i = 0; i<arr.Length; i++)
+                {
+                    arr[i] = rnd.Next(1, 9);
+                }
+                for (int j = 0; j < Inventary.Length; j++)
+                {
+                    if (Inventary[j] == 0 && a != 0)
+                    {
+                        Inventary[j] = arr[a - 1];
+                        a--;
+
+                    }
+                    else if (Inventary[11] != 0)
+                    {
+                        break;
+                    }
+                    
+                }
+                TakeChest = false;
+            }
+        }
+                
+        static void Load()
+        {         
+            //LoadMap   
+            Map = LoadMap.Map.Start.Select(r => r.ToCharArray()).ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.SetBufferSize(100, Console.BufferHeight);
+            Console.SetWindowSize(100, Console.WindowHeight);
+            Load();
+            SetSize();
+            CreateChest();
+            Draw();
+        }
+    }
+}
